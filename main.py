0b6a588ae922d7f1e30bafb1e7115e2663092d66
Majid Darvishan <majiddarvishan@outlook.com>
2020-11-13 19:15:36 +0330

    Add .env

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from code.heroku import run_app
 app = run_app()
 
 # A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+# @app.route('/')
+# def index():
+#     return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
