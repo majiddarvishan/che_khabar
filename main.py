2c56dba01c09086b53f034512ff420a5455cb923
Majid Darvishan <majiddarvishan@outlook.com>
2020-11-13 18:29:21 +0330

    Add main route

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from code.heroku import run_app
 
 app = run_app()
 
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
