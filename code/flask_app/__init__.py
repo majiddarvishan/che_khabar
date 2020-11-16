8306a6497b004bfafb06d3cbe872fc996b33a5a0
Majid Darvishan <majiddarvishan@outlook.com>
2020-11-16 23:42:47 +0330

    Update routes

--- code/flask_app/__init__.py
+++ code/flask_app/__init__.py
@@ -9,33 +9,14 @@ def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object("config.Config")
+    # app.config['JSON_SORT_KEYS'] = False
 
     db.init_app(app)
 
-    # Create database tables for our data models
     with app.app_context():
-        from . import routes  # Import routes
+        from . import routes  
         
         db.create_all() 
-
-    # app.config['JSON_SORT_KEYS'] = False
-    api = Api(app)
-
-    # api.add_resource(Quote, "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:id>")
-
-    from . import user_profile
-    api.add_resource(user_profile.UserProfile, '/users', 
-                                               endpoint='/users', 
-                                               strict_slashes=False)
-    # api.add_resource(user_profile.UserProfile, '/users/<user_id>', endpoint='/users/<user_id>')
-
-    from . import advertisement_profile
-    api.add_resource(advertisement_profile.AdvertisementProfile, '/advertisements', 
-                                                                 endpoint='/advertisements', 
-                                                                 strict_slashes=False)
-    # api.add_resource(advertisement_profile.AdvertisementProfile, '/advertisements/<advertisement_id>', endpoint='/advertisements/<advertisement_id>')
-
-    # db.create_all()  # Create database tables for our data models
 
     from . import my_json_encoder
     app.json_encoder = my_json_encoder.MyJSONEncoder
--- code/flask_app/routes.py
+++ code/flask_app/routes.py
@@ -3,30 +3,47 @@ from datetime import datetime as dt
 
 from flask import current_app as app
 from flask import make_response, redirect, render_template, request, url_for
+from flask_restful import Resource, Api
 
 from .models import User, db
 
-@app.route("/", methods=["GET"])
-def user_records():
-    """Create a user via query string parameters."""
-    firstname = request.args.get("firstname")
-    lastname = request.args.get("lastname")
-    email = request.args.get("email")
-    if email:
-        existing_user = User.query.filter(
-            User.email == email
-        ).first()
-        if existing_user:
-            return make_response(f"{firstname} {lastname} ({email}) already created!")
-        new_user = User(
-            firstname=firstname,
-            lastname=lastname,
-            email=email,
-            created=dt.now(),
-            bio="In West Philadelphia born and raised, \
-            on the playground is where I spent most of my days"
-        )  # Create an instance of the User class
-        db.session.add(new_user)  # Adds new User record to database
-        db.session.commit()  # Commits all changes
-        redirect(url_for("user_records"))
-    return render_template("users.jinja2", users=User.query.all(), title="Show Users")
+# @app.route("/", methods=["GET"])
+# def user_records():
+#     """Create a user via query string parameters."""
+#     firstname = request.args.get("firstname")
+#     lastname = request.args.get("lastname")
+#     email = request.args.get("email")
+#     if email:
+#         existing_user = User.query.filter(
+#             User.email == email
+#         ).first()
+#         if existing_user:
+#             return make_response(f"{firstname} {lastname} ({email}) already created!")
+#         new_user = User(
+#             firstname=firstname,
+#             lastname=lastname,
+#             email=email,
+#             created=dt.now(),
+#             bio="In West Philadelphia born and raised, \
+#             on the playground is where I spent most of my days"
+#         )  # Create an instance of the User class
+#         db.session.add(new_user)  # Adds new User record to database
+#         db.session.commit()  # Commits all changes
+#         redirect(url_for("user_records"))
+#     return render_template("users.jinja2", users=User.query.all(), title="Show Users")
+
+api = Api(app)
+
+# api.add_resource(Quote, "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:id>")
+
+from . import user_profile
+api.add_resource(user_profile.UserProfile, '/users', 
+                                            endpoint='/users', 
+                                            strict_slashes=False)
+# api.add_resource(user_profile.UserProfile, '/users/<user_id>', endpoint='/users/<user_id>')
+
+from . import advertisement_profile
+api.add_resource(advertisement_profile.AdvertisementProfile, '/advertisements', 
+                                                                endpoint='/advertisements', 
+                                                                strict_slashes=False)
+# api.add_resource(advertisement_profile.AdvertisementProfile, '/advertisements/<advertisement_id>', endpoint='/advertisements/<advertisement_id>')
